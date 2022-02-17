1b0ada191577985ab68e7b1ddbbc3151558ad0b7
Kelvin <94995616+xmurderinc@users.noreply.github.com>
2022-02-17 11:09:25 -0500

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -5,6 +5,8 @@ ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten',
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
          'Queen':10, 'King':10, 'Ace':11}
 
+playing = True
+
 class Card:
     
     def __init__(self,suit,rank):
